367c094c3cb03f73c7b646b62f2967262440daa9
chamil <chamil@emarketingeye.com>
2015-07-06 09:20:11 +0530

    first commit

---
+++ sites/all/themes/eme/template.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * implements hook_menu_link
+ * @param  [type] $variables [description]
+ * @return [type]            [description]
+ */
+function eme_menu_link($variables) {
+
+  $link = $variables['links'];
+
+  $html = "<div>\n";
+  $html .= "<ul>\n";
+
+
+  foreach ($variables['links'] as $link) {
+
+  /************************
+  *echo url( $link['href'], array('absolute' => true, 'alias' => false ));
+  *url() er den funtion som bruges til at hive den rene url sti ud.
+  ************************/
+
+  $html .= "<li>";
+
+  $html .= '<a href="'.url( $link['href'], array('alias' => false )).'"><strong><strong>' .    $link['title']. '</strong></strong></a>';
+  $html .= '<br>';
+  $html .= "</li>";
+}
+
+$html .= "  </ul>\n";
+$html .= "</div>\n";
+
+}
+
+/**
+ *
+ * implements hook_links__menu
+ * [eme_links__topbar_main_menu description]
+ * @param  [type] $variables [description]
+ * @return [type]            [description]
+ */
+function eme_links__topbar_main_menu($variables) {
+
+  $link = $variables['links'];
+
+  $html = "<div>\n";
+  $html .= "<ul>\n";
+
+
+  foreach ($variables['links'] as $link) {
+
+  /************************
+  *echo url( $link['href'], array('absolute' => true, 'alias' => false ));
+  *url() er den funtion som bruges til at hive den rene url sti ud.
+  ************************/
+
+  $html .= "<li>";
+
+  $html .= '<a href="'.url( $link['href'], array('alias' => false )).'"><strong><strong>' .    $link['title']. '</strong></strong></a>';
+  $html .= '<br>';
+  $html .= "</li>";
+}
+
+$html .= "  </ul>\n";
+$html .= "</div>\n";
+
+return $html;
+}
